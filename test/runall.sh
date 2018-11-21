803e0aa56ec7c4f38bc50538227b66d88cbbd3cd
Tsugikazu Shibata <tgkzsh@gmail.com>
2018-11-21 23:50:42 +0900

    add new directories and test results

--- test/runall.sh
+++ test/runall.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
-PAHT=$PATH:`pwd`/..   # add command dir to $PATH
+PATH=$PATH:`pwd`/../:   # add command dir to $PATH
 for i in *
 do
   if [ -d $i ]; then
     echo "Processing ", $i
+    if [ -f kanalist.txt ]; then
+       cp kanalist.txt kanalist-old.txt
+    fi
     cd $i; klist >kanalist.txt
     cd ..
   fi
