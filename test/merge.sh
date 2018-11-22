02bed7866b19acc26c3ecd8ce27c8713943217c2
Tsugikazu Shibata <tgkzsh@gmail.com>
2018-11-23 00:01:34 +0900

    add all the directories of open source guide and merge.sh

---
+++ test/merge.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+PATH=$PATH:`pwd`/../:   # add command dir to $PATH
+for i in *
+do
+  if [ -d $i ]; then
+    echo "Processing ", $i
+    if [ -f $i/kanalist.txt ]; then
+       cat $i/kanalist.txt >>merged.txt
+    fi
+  fi
+  cat merged.txt |sort|uniq >kanamerge.txt
+done 
+rm merged.txt
